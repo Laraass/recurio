c1d33beb5e9484d8e2bfdf299a31eb65283257a5
Lara <lara.salman01@hotmail.com>
2025-10-06 16:15:57 +0200

    feat: create logo component

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,10 +1,15 @@
-export function Logo() {
+import { Icon } from "@iconify/react";
 
-    return (
-    <>
-    </>
-    )
-
+function Logo() {
+  return (
+    <div className="flex flex-row items-center">
+      <span className="text-4xl font-radley">RECURI</span>
+      <Icon
+        icon="streamline:subscription-cashflow"
+        className="text-neutral-950 size-[1.625rem]"
+      />
+    </div>
+  );
 }
 
 export default Logo;
