a8f9f3742e107799273d0ce8bd943f3c03508fb9
Lara <lara.salman01@hotmail.com>
2025-10-08 15:45:59 +0200

    chore: format code in DeleteButton.tsx

--- frontend/src/components/DeleteButton.tsx
+++ frontend/src/components/DeleteButton.tsx
@@ -1,15 +1,18 @@
-import { Icon } from "@iconify/react"
+import { Icon } from "@iconify/react";
 
 interface DeleteButtonProps {
-    onClick?: () => void; 
+  onClick?: () => void;
 }
 
-const DeleteButton: React.FC<DeleteButtonProps> = ({onClick }) => {
-    return(
-        <button title="Delete" onClick={onClick}>
-        <Icon icon="mingcute:delete-fill" className="size-5 text-neutral-925 hover:text-red-500 active:text-red-500 cursor-pointer transition-colors duration-200"></Icon>
-        </button>
-    )
-}
+const DeleteButton: React.FC<DeleteButtonProps> = ({ onClick }) => {
+  return (
+    <button title="Delete" onClick={onClick}>
+      <Icon
+        icon="mingcute:delete-fill"
+        className="size-5 text-neutral-925 hover:text-red-500 active:text-red-500 cursor-pointer transition-colors duration-200"
+      ></Icon>
+    </button>
+  );
+};
 
-export default DeleteButton
+export default DeleteButton;
