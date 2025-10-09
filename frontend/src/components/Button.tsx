b5ad9b28d19a74f658620bf85a605e908ac7da58
Lara <lara.salman01@hotmail.com>
2025-10-09 14:45:37 +0200

    chore: format code in Button.tsx

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,34 +1,31 @@
 interface ButtonProps {
-    onClick?: () => void;
-    children: React.ReactNode;
-    variant?: "default" | "dark";
-    type?: "button" | "submit" | "reset";
-    className?: string;
-
+  onClick?: () => void;
+  children: React.ReactNode;
+  variant?: "default" | "dark";
+  type?: "button" | "submit" | "reset";
+  className?: string;
 }
 const Button: React.FC<ButtonProps> = ({
-    children,
-    onClick,
-    variant = "default",
-    type = "button",
-    className = "rounded-",
-
+  children,
+  onClick,
+  variant = "default",
+  type = "button",
+  className = "rounded-",
 }) => {
-    const styles = {
-        default: " bg-primary hover:bg-accent active:bg-accent",
-        dark: "bg-neutral-950 text-neutral-50 hover:text-primary active:text-primary", 
-    }
+  const styles = {
+    default: " bg-primary hover:bg-accent active:bg-accent",
+    dark: "bg-neutral-950 text-neutral-50 hover:text-primary active:text-primary",
+  };
 
-    return(
-        <button
-        onClick={onClick}
-        type={type}
-        className={`flex items-center justify-center px-5.5 py-4 rounded-xl text-base font-bold cursor-pointer transition-colors duration-200 ${styles[variant]} ${className}`}
-
-        >
-            {children}
-        </button>
-    )
-}
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={`flex items-center justify-center px-5.5 py-4 rounded-xl text-base font-bold cursor-pointer transition-colors duration-200 ${styles[variant]} ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
