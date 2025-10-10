32ab2d8025d682217e4354d1d3174e39ff829478
Lara <lara.salman01@hotmail.com>
2025-10-10 13:22:20 +0200

    feat: create searchbar

---
+++ frontend/src/components/Searchbar.tsx
@@ -0,0 +1,44 @@
+import { Icon } from "@iconify/react";
+import { useState, type ChangeEvent } from "react";
+
+export type SearchbarProps = {
+    onSearch: (value: string) => void;
+}
+
+const Searchbar = (props: SearchbarProps) => {
+    const {onSearch} = props;
+    const placeholderValue = "Search..."
+    const [value, setValue] = useState(placeholderValue);
+
+    const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { target } = event;
+        setValue(target.value)
+        console.log({value})
+    }
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            onSearch(value);
+            console.log({value})
+        }
+    }
+    return(
+        <div className="flex items-center gap-1 px-3 py-2 rounded-3xl border border-neutral-400 
+            placeholder-gray-500 placeholder-opacity-100 transition-colors 
+            duration-200 focus:outline-none focus:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] max-w-3xs">
+                <input type={"search"}
+                name={"search"} 
+                placeholder={placeholderValue}
+                className="focus:outline-none w-full"
+                onChange={searchHandler}
+                onKeyDown={handleKeyDown}
+                >
+                
+                </input>
+                <Icon icon="bitcoin-icons:search-filled" className="size-7.5 text-gray-500" ></Icon>
+                
+        </div>
+
+    )
+}
+
+export default Searchbar
