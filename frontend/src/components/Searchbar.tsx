df4058cde4f38cac56e4c46aaad29977106275de
Lara <lara.salman01@hotmail.com>
2025-10-10 14:38:08 +0200

    feat: replace default search cancel button with iconify icon

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -2,39 +2,56 @@ import { Icon } from "@iconify/react";
 import { useState, type ChangeEvent } from "react";
 
 export type SearchbarProps = {
-    onSearch: (value: string) => void;
-    placeholder?: string;
-}
+  onSearch: (value: string) => void;
+  placeholder?: string;
+};
 
-const Searchbar = ({ onSearch, placeholder = "Search... "}: SearchbarProps) => {
-    const [value, setValue] = useState("");
+const Searchbar = ({
+  onSearch,
+  placeholder = "Search... ",
+}: SearchbarProps) => {
+  const [value, setValue] = useState("");
 
-    const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value)
+  const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSearch(value);
     }
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === "Enter") {
-            onSearch(value);
-        }
-    }
-    return(
-        <div className="flex items-center gap-1 px-4 py-2 rounded-3xl border border-neutral-400 
-            placeholder-gray-500 placeholder-opacity-100 transition-colors 
-            duration-200 focus:outline-none focus:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] max-w-2xs">
-                <input type={"search"}
-                name={"search"} 
-                placeholder={placeholder}
-                className="focus:outline-none w-full"
-                onChange={searchHandler}
-                onKeyDown={handleKeyDown}
-                >
-                
-                </input>
-                <Icon icon="bitcoin-icons:search-filled" className="size-7.5 text-gray-500 cursor-pointer hover:text-neutral-925 active:text-neutral-925 " onClick={() => onSearch(value)}></Icon>
-                
-        </div>
+  };
+  return (
+    <div
+      className="flex items-center gap-1 px-4 py-2 rounded-3xl border border-neutral-400 
+            placeholder-gray-500 placeholder-opacity-100 transition-colors max-w-2xs
+            duration-200"
+    >
+      <input
+        type={"search"}
+        name={"search"}
+        placeholder={placeholder}
+        value={value}
+        className="focus:outline-none w-full"
+        onChange={searchHandler}
+        onKeyDown={handleKeyDown}
+      ></input>
+      <div className="flex items-center">
+        {value && (
+          <Icon
+            icon="mdi:close"
+            className=" text-gray-500 hover:text-red-500 cursor-pointer size-5"
+            onClick={() => setValue("")}
+          />
+        )}
 
-    )
-}
+        <Icon
+          icon="bitcoin-icons:search-filled"
+          className="size-7 text-gray-500 cursor-pointer hover:text-neutral-925 active:text-neutral-925"
+          onClick={() => onSearch(value)}
+        ></Icon>
+      </div>
+    </div>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
