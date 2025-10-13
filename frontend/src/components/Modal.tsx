73a861fa9f956d336dbf173435a89c6bf57ca9c2
Lara <lara.salman01@hotmail.com>
2025-10-13 13:53:26 +0200

    feat: add EditSub variant

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -10,6 +10,11 @@ interface ModalProps {
 
   company?: string;
   image?: string;
+
+  defaultValue?: {
+    description?: string;
+    price?: string;
+  };
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -19,6 +24,7 @@ const Modal: React.FC<ModalProps> = ({
   onSubmit,
   company,
   image,
+  defaultValue,
 }) => {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
@@ -27,14 +33,17 @@ const Modal: React.FC<ModalProps> = ({
     if (!open) {
       setDescription("");
       setPrice("");
+    } else if (variant === "EditSub") {
+      setDescription(defaultValue?.description || "");
+      setPrice(defaultValue?.price || "");
     }
-  }, [open]);
+  }, [open, variant, defaultValue]);
 
   if (!open) return null;
 
   return (
     <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center px-4">
-      <div className="flex flex-col items-start gap-6 p-6 rounded-xl min-w-xs bg-neutral-50 shadow-[0_0_20px_0_rgba(0,0,0,0.25)] ">
+      <div className="flex flex-col items-start gap-6 p-6 rounded-xl w-full max-w-92 bg-neutral-50 shadow-[0_0_20px_0_rgba(0,0,0,0.25)] ">
         {variant === "AddSub" && (
           <>
             <h2 className="text-2xl font-semibold">Add subscription</h2>
@@ -71,6 +80,50 @@ const Modal: React.FC<ModalProps> = ({
 
               <div className="flex items-start gap-4">
                 <Button type="submit"> Add new </Button>
+                <Button type="button" variant="dark" onClick={onClose}>
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </>
+        )}
+
+        {variant === "EditSub" && (
+          <>
+            <h2 className="text-2xl font-semibold">Edit subscription</h2>
+
+            <div className="flex gap-3 items-center">
+              {image && <img src={image} alt={company} className="size-12" />}
+
+              {company && <p className="text-xl font-medium">{company}</p>}
+            </div>
+
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                onSubmit({ description, price });
+                onClose();
+              }}
+              className="flex flex-col w-full gap-6"
+            >
+              <InputField
+                value={description}
+                title="Description"
+                type="text"
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Enter description..."
+              />
+
+              <InputField
+                value={price}
+                title="Price (SEK)"
+                type="number"
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Enter price..."
+              />
+
+              <div className="flex items-start gap-4">
+                <Button type="submit"> Save changes </Button>
                 <Button type="button" variant="dark" onClick={onClose}>
                   Cancel
                 </Button>
