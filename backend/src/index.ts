35943312347d737fb4d2a96aa71054c2203d54ed
Lara <lara.salman01@hotmail.com>
2025-10-14 13:32:16 +0200

    feat: create mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
 import fastify from "fastify";
+import { connectDB } from "./plugins/db";
+
+dotenv.config();
 
 const server = fastify()
 
@@ -6,11 +10,16 @@ server.get("/", function (request, reply) {
     reply.send({ hello: "world" })
 })
 
-server.listen({ port: 3000}, function (err, PORT) {
-    if (err) {
-        server.log.error(err)
-        process.exit(1)
+const start = async () => {
+    try {
+        await connectDB();
+        console.log("Database connected")
+        const PORT = await server.listen({ port: Number(process.env.PORT) || 3000, host: "0.0.0.0" })
+        console.log(`Server is running on ${PORT}`)
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
     }
+};
 
-    console.log(`Server is now listening on ${PORT}`)
-})
+start();
