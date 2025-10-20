6eae649260101f485457c0965ca0d360e2d5edac
Lara <lara.salman01@hotmail.com>
2025-10-20 16:50:47 +0200

    chore: format code in userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,15 +1,13 @@
 import { FastifyInstance } from "fastify";
 
 export default async function userRoutes(fastify: FastifyInstance) {
-    
-    fastify.post("/users/register", async (request, reply) => {});
+  fastify.post("/users/register", async (request, reply) => {});
 
-    fastify.post("/users/login", async (request, reply) => {});
+  fastify.post("/users/login", async (request, reply) => {});
 
-    fastify.post("/users/logout", async (request, reply) => {});
+  fastify.post("/users/logout", async (request, reply) => {});
 
-    fastify.get("/users/profile", async (request, reply) => {});
+  fastify.get("/users/profile", async (request, reply) => {});
 
-    fastify.patch("/users/subscribe", async (request, reply) => {})
-
+  fastify.patch("/users/subscribe", async (request, reply) => {});
 }
