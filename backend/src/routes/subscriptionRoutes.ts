86be815ab01a3061e0015639785fee5eb72e3108
Lara <lara.salman01@hotmail.com>
2025-10-20 16:50:21 +0200

    chore: format code in subscriptionRoutes.ts

--- backend/src/routes/subscriptionRoutes.ts
+++ backend/src/routes/subscriptionRoutes.ts
@@ -1,17 +1,24 @@
 import { FastifyInstance } from "fastify";
 
 export default async function subscriptionRoutes(fastify: FastifyInstance) {
+  fastify.get("/subscriptions", async (request, reply) => {});
 
-    fastify.get("/subscriptions", async (request, reply) => {});
+  fastify.get("/users/:userId/subscriptions", async (request, reply) => {});
 
-    fastify.get("/users/:userId/subscriptions", async (request, reply) => {});
+  fastify.post("/users/:userId/subscriptions", async (request, reply) => {});
 
-    fastify.post("/users/:userId/subscriptions", async (request, reply) => {});
+  fastify.patch(
+    "/users/:userId/subscriptions/:id",
+    async (request, reply) => {}
+  );
 
-    fastify.patch("/users/:userId/subscriptions/:id", async (request, reply) => {});
+  fastify.delete(
+    "/users/:userId/subscriptions/:id",
+    async (request, reply) => {}
+  );
 
-    fastify.delete("/users/:userId/subscriptions/:id", async (request, reply) => {});
-
-    fastify.get("/users/:userId/subscriptions/statistics", async (request, reply) => {});
-
+  fastify.get(
+    "/users/:userId/subscriptions/statistics",
+    async (request, reply) => {}
+  );
 }
