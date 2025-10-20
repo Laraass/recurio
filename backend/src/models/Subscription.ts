82c3ebaf7cbcba514cdf8a6ef0fe47a39cabe38f
Lara <lara.salman01@hotmail.com>
2025-10-20 13:38:14 +0200

    chore: format code in Subscription.ts

--- backend/src/models/Subscription.ts
+++ backend/src/models/Subscription.ts
@@ -2,42 +2,42 @@ import mongoose, { Document, Schema } from "mongoose";
 import { IUser } from "./User";
 
 export interface ISubscription extends Document {
-    company: string;
-    price: number;
-    description?: string;
-    image?: string;
-    userId?: IUser["_id"];
-    createdAt: Date;
-    updatedAt: Date;
+  company: string;
+  price: number;
+  description?: string;
+  image?: string;
+  userId?: IUser["_id"];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const SubscriptionSchema = new Schema<ISubscription>(
-    {
-        company: { 
-            type: String,
-            required: true,
-            trim: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        description: {
-            type: String,
-            trim: true,
-        },
-        image: {
-            type: String,
-        },
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        },
+  {
+    company: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    { timestamps: true }
-)
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model<ISubscription>(
-    "Subscription",
-    SubscriptionSchema
-)
+  "Subscription",
+  SubscriptionSchema
+);
