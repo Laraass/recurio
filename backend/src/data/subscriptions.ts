4c09a88c3884b57031def6f98d35d063dc2dac39
Lara <lara.salman01@hotmail.com>
2025-10-21 21:04:22 +0200

    feat: add subscription data for seeding backend

---
+++ backend/src/data/subscriptions.ts
@@ -0,0 +1,217 @@
+export const subscriptions = [
+  {
+    company: "Netflix",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/ideQwN5lBE/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "HBO Max",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idKKo6p4ks/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "EA Play",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/id1MBpIUH-/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Disney+",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idhQlYRiX2/w/160/h/160/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Prime Video",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idXRXV6qi8/w/200/h/200/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Xbox Game Pass",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/id1jbGiFD1/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "PlayStation Plus",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idsn73YDWv/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Hulu",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/id4KRloCCr/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "YouTube",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idVfYwcuQz/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Viaplay",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idQEd3oGZG/w/480/h/480/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Apple TV",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idNrank8JY/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Ubisoft+",
+    category: "Entertainment",
+    image:
+      "https://cdn.brandfetch.io/idtVsonT9X/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Spotify",
+    category: "Music",
+    image:
+      "https://cdn.brandfetch.io/id20mQyGeY/w/320/h/320/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Apple Music",
+    category: "Music",
+    image:
+      "https://cdn.brandfetch.io/id_yBTuraI/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "SoundCloud",
+    category: "Music",
+    image:
+      "https://cdn.brandfetch.io/id3ytDFop3/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Google Drive",
+    category: "Cloud",
+    image:
+      "https://cdn.iconscout.com/icon/free/png-256/free-google-drive-logo-icon-svg-download-png-2476481.png?f=webp",
+  },
+  {
+    company: "Dropbox",
+    category: "Cloud",
+    image:
+      "https://cdn.brandfetch.io/idY3kwH_Nx/w/400/h/400/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "iCloud",
+    category: "Cloud",
+    image:
+      "https://images.seeklogo.com/logo-png/42/2/apple-icloud-logo-png_seeklogo-426388.png",
+  },
+  {
+    company: "Amazon Prime",
+    category: "Shopping",
+    image:
+      "https://brandlogos.net/wp-content/uploads/2021/04/amazon-prime-logo.png",
+  },
+  {
+    company: "Aftonbladet",
+    category: "News",
+    image:
+      "https://cdn.brandfetch.io/idNhWSBClN/w/1024/h/1024/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Expressen",
+    category: "News",
+    image:
+      "https://cdn.brandfetch.io/idgjOH3loK/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Sportbladet",
+    category: "News",
+    image:
+      "https://play-lh.googleusercontent.com/M7K7bIKKNgs9uzvwOzua29i9H0ETp0IMwArqIUNInqnhPxBXWmyV2MKAKa03o0m-TQ",
+  },
+  {
+    company: "Dagens Nyheter",
+    category: "News",
+    image:
+      "https://cdn.brandfetch.io/idy_Nd-prm/w/960/h/960/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Notion",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idYnkdM3Ni/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Slack",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idJ_HhtG0Z/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Microsoft 365",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idsWBrtc_i/w/400/h/400/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Canva Pro",
+    category: "Productivity",
+    image:
+      "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxo4K81Ei7WzcnqEk8W.MgwaH94jRmtJPRlv4F_Z0WGxl4DqVmtd6.Y76H4VGL9nDXhYbs1dM3Ct_E.XEwVjczMY-&format=source",
+  },
+  {
+    company: "Jira",
+    category: "Productivity",
+    image:
+      "https://images.seeklogo.com/logo-png/37/1/jira-logo-png_seeklogo-373036.png",
+  },
+  {
+    company: "Google Workspace",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idpydcX_6x/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Trello",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idToc8bDY1/w/400/h/400/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Adobe Creative Cloud",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idkrQGARPW/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Bokus",
+    category: "Productivity",
+    image:
+      "https://cdn.brandfetch.io/idTkOAfBPP/w/180/h/180/theme/dark/icon.png?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+  {
+    company: "Adlibris",
+    category: "Productivity",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIzNlLEobCcQR3PIqCfzCB88oicZsune62hQ&s",
+  },
+  {
+    company: "SATS",
+    category: "Fitness",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0VViEWLggSz5PJ00jNyAoOCfSc7o8mW8j1w&s",
+  },
+  {
+    company: "Nordic Wellness",
+    category: "Fitness",
+    image: "https://www.xcen.se/media/wysiwyg/nordic-wellness-logotyp.png",
+  },
+  {
+    company: "Fitness24Seven",
+    category: "Fitness",
+    image:
+      "https://cdn.brandfetch.io/idCB9GIKl-/w/1080/h/1080/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B",
+  },
+];
