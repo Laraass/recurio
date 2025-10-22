da6e101ca84bc0b3ed109f55722f62b8ecb70feb
Lara <lara.salman01@hotmail.com>
2025-10-22 13:56:30 +0200

    feat: protect user routes with middleware

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -6,15 +6,16 @@ import {
   registerUser,
   subscribeEmail,
 } from "../controllers/userController";
+import { auth } from "../middleware/auth";
 
 export default async function userRoutes(fastify: FastifyInstance) {
   fastify.post("/users/register", registerUser);
 
   fastify.post("/users/login", loginUser);
 
-  fastify.post("/users/logout", logoutUser);
+  fastify.post("/users/logout", { preHandler: auth }, logoutUser);
 
-  fastify.get("/users/profile", getProfile);
+  fastify.get("/users/profile", { preHandler: auth }, getProfile);
 
-  fastify.patch("/users/subscribe", subscribeEmail);
+  fastify.patch("/users/subscribe", { preHandler: auth }, subscribeEmail);
 }
