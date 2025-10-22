599411effaddd54300cda476bd631e3a10897efc
Lara <lara.salman01@hotmail.com>
2025-10-22 12:24:24 +0200

    feat: connect subscription routes to subscription controller functions

--- backend/src/routes/subscriptionRoutes.ts
+++ backend/src/routes/subscriptionRoutes.ts
@@ -1,21 +1,22 @@
 import { FastifyInstance } from "fastify";
+import {
+  addSubscription,
+  deleteSubscription,
+  editSubscription,
+  listAllSubscriptions,
+  listUserSubscriptions,
+} from "../controllers/subscriptionController";
 
 export default async function subscriptionRoutes(fastify: FastifyInstance) {
-  fastify.get("/subscriptions", async (request, reply) => {});
+  fastify.get("/subscriptions", listAllSubscriptions);
 
-  fastify.get("/users/:userId/subscriptions", async (request, reply) => {});
+  fastify.get("/users/:userId/subscriptions", listUserSubscriptions);
 
-  fastify.post("/users/:userId/subscriptions", async (request, reply) => {});
+  fastify.post("/users/:userId/subscriptions", addSubscription);
 
-  fastify.patch(
-    "/users/:userId/subscriptions/:id",
-    async (request, reply) => {}
-  );
+  fastify.patch("/users/:userId/subscriptions/:id", editSubscription);
 
-  fastify.delete(
-    "/users/:userId/subscriptions/:id",
-    async (request, reply) => {}
-  );
+  fastify.delete("/users/:userId/subscriptions/:id", deleteSubscription);
 
   fastify.get(
     "/users/:userId/subscriptions/statistics",
