6f2860b1c00a0bcddb013dfcc87a0b0bf382b3f8
Lara <lara.salman01@hotmail.com>
2025-10-22 13:56:06 +0200

    feat: protect subscription routes with middleware

--- backend/src/routes/subscriptionRoutes.ts
+++ backend/src/routes/subscriptionRoutes.ts
@@ -6,20 +6,38 @@ import {
   listAllSubscriptions,
   listUserSubscriptions,
 } from "../controllers/subscriptionController";
+import { auth } from "../middleware/auth";
 
 export default async function subscriptionRoutes(fastify: FastifyInstance) {
   fastify.get("/subscriptions", listAllSubscriptions);
 
-  fastify.get("/users/:userId/subscriptions", listUserSubscriptions);
+  fastify.get(
+    "/users/:userId/subscriptions",
+    { preHandler: auth },
+    listUserSubscriptions
+  );
 
-  fastify.post("/users/:userId/subscriptions", addSubscription);
+  fastify.post(
+    "/users/:userId/subscriptions",
+    { preHandler: auth },
+    addSubscription
+  );
 
-  fastify.patch("/users/:userId/subscriptions/:id", editSubscription);
+  fastify.patch(
+    "/users/:userId/subscriptions/:id",
+    { preHandler: auth },
+    editSubscription
+  );
 
-  fastify.delete("/users/:userId/subscriptions/:id", deleteSubscription);
+  fastify.delete(
+    "/users/:userId/subscriptions/:id",
+    { preHandler: auth },
+    deleteSubscription
+  );
 
   fastify.get(
     "/users/:userId/subscriptions/statistics",
+    { preHandler: auth },
     async (request, reply) => {}
   );
 }
