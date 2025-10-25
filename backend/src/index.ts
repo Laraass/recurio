2eaf04bcf79cc404e91551020d0659b5cde74a71
Lara <lara.salman01@hotmail.com>
2025-10-25 17:06:26 +0200

    fix: allow cors for frontend and production domains

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,7 @@ const start = async () => {
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    preflight: true,
   });
 
   server.get("/", function (request, reply) {
