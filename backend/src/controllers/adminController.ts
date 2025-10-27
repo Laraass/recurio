d2980624d37b42c7a87edce6475bc0d81b969abe
Lara <lara.salman01@hotmail.com>
2025-10-27 10:45:32 +0100

    feat: create function to fetch all users

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -1,5 +1,12 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { User } from "../models/User";
 
 export const listAllUsers = async (request: FastifyRequest, reply: FastifyReply) => {
-
+    try {
+        const users = await User.find().select("-password");
+        reply.send({ users })
+    } catch (error) {
+        reply.status(500).send({ error: "Failed to fetch users", details: error })
+    }
 }
+
