916bdc801460db10586d5d7d1a91528ef7682144
Lara <lara.salman01@hotmail.com>
2025-10-27 23:52:34 +0100

    feat: add state and useEffect to TotalSum component

--- frontend/src/components/TotalSum.tsx
+++ frontend/src/components/TotalSum.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+
 interface TotalSumProps {
   total?: number;
 }
 
-const TotalSum: React.FC<TotalSumProps> = ({ total = 0 }) => {
+const TotalSum: React.FC<TotalSumProps> = () => {
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+
+
+    
+  }, []);
+
   return (
     <div className="flex flex-col p-6 bg-primary text-neutral-50 gap-2 rounded-xl max-w-md">
       <p className="text-xl font-medium">Total cost this month</p>
