a2276d02235f40e2e5cd7f38cd6a0dfb95615d8f
Lara <lara.salman01@hotmail.com>
2025-10-27 14:21:45 +0100

    chore: fomat documents

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,24 +1,24 @@
 import nodemailer from "nodemailer";
 
 interface EmailContent {
-    to: string;
-    subject: string,
-    text: string;
+  to: string;
+  subject: string;
+  text: string;
 }
 
-export const sendEmail = async ({ to, subject, text}: EmailContent) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.ADMIN_USER,
-            pass: process.env.ADMIN_PASS,
-        },
-    })
+export const sendEmail = async ({ to, subject, text }: EmailContent) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.ADMIN_USER,
+      pass: process.env.ADMIN_PASS,
+    },
+  });
 
-    await transporter.sendMail({
-        from: process.env.ADMIN_USER,
-        to,
-        subject,
-        text,
-    })
-}
+  await transporter.sendMail({
+    from: process.env.ADMIN_USER,
+    to,
+    subject,
+    text,
+  });
+};
