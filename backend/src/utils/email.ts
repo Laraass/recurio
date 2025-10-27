f6a11ac99ea9799430eab44cdf6e77ca4fd64d64
Lara <lara.salman01@hotmail.com>
2025-10-27 12:49:18 +0100

    feat: install and set up nodemailer

---
+++ backend/src/utils/email.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+interface EmailContent {
+    to: string;
+    subject: string,
+    text: string;
+}
+
+export const sendEmail = async ({ to, subject, text}: EmailContent) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.ADMIN_USER,
+            pass: process.env.ADMIN_PASS,
+        },
+    })
+
+    await transporter.sendMail({
+        from: process.env.ADMIN_USER,
+        to,
+        subject,
+        text,
+    })
+}
