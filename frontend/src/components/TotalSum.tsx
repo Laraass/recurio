319b1160ee6cc07a2ce23226be8887cd4b9575cc
Lara <lara.salman01@hotmail.com>
2025-10-28 13:10:35 +0100

    feat: fetch and display user subscriptions in home page

--- frontend/src/components/TotalSum.tsx
+++ frontend/src/components/TotalSum.tsx
@@ -33,7 +33,7 @@ const TotalSum: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col p-6 bg-primary text-neutral-50 gap-2 rounded-xl max-w-md">
+    <div className="flex flex-col p-6 bg-primary text-neutral-50 gap-2 rounded-xl">
       <p className="text-xl font-medium">Total cost this month</p>
       <p className="text-5xl font-semibold">{total} kr</p>
     </div>
