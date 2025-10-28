5e800bbece2ab5da9a84d7cc6a58c2ea58b1211b
Lara <lara.salman01@hotmail.com>
2025-10-28 13:58:48 +0100

    feat: fetch and display all subscriptions

--- frontend/src/pages/AddSubscription.tsx
+++ frontend/src/pages/AddSubscription.tsx
@@ -1,17 +1,44 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
+import api from "../api/axios";
+
+interface Subscription {
+  _id: string;
+  company: string;
+  image?: string;
+}
 
 const AddSubscription: React.FC = () => {
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
 
+  useEffect(() => {
+    const fetchAllSubscriptions = async () => {
+      try {
+        const res = await api.get("/subscriptions");
+        setSubscriptions(res.data.subscriptions);
+      } catch (error) {
+        console.error("Failed to fetch all subscriptions", error);
+      }
+    };
+
+    fetchAllSubscriptions();
+  }, []);
 
   return (
     <div className="flex flex-col items-center pt-8 max-w-6xl mx-auto">
-      <div className="flex flex-col items-center sm:gap-3 sm:px-6 sm:py-9 sm:w-full sm:border sm:border-neutral-400 sm:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] sm:rounded-xl">
+      <div className="flex flex-col items-center sm:gap-3 sm:px-6 sm:py-9 w-full sm:border sm:border-neutral-400 sm:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] sm:rounded-xl">
         <div className="flex flex-col gap-3 w-full max-w-110">
           <h1 className="text-2xl font-semibold">All subscriptions</h1>
 
-          <div className="flex flex-col overflow-auto max-h-[26rem] scrollbar-none rounded-xl border border-neutral-400">
-            <Card variant={"Company"} />
-          
+          <div className="flex flex-col overflow-auto sm:max-h-[40rem] scrollbar-none rounded-xl border border-neutral-400">
+            {subscriptions.map((subscription) => (
+              <Card
+                variant="Company"
+                key={subscription._id}
+                company={subscription.company}
+                image={subscription.image}
+              />
+            ))}
           </div>
         </div>
       </div>
