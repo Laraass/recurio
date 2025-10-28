cbdeeb484836de789744997a41a69fa1fca2e9a4
Lara <lara.salman01@hotmail.com>
2025-10-28 22:08:07 +0100

    feat: allow patch route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const server = fastify();
 const start = async () => {
   await server.register(cors, {
     origin: ["http://localhost:5173", "https://recurio-app.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   });
