7bef2a68b7e8ae7d88ac9bd2797426e3b5d80337
Lara <lara.salman01@hotmail.com>
2025-10-29 15:33:45 +0100

    feat: add navigation to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from "react";
 import NavbarButton from "./NavbarButton";
 import Logo from "./Logo";
+import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const [activePage, setActivePage] = useState("home");
-  const [admin, setAdmin] = useState(false)
+  const [admin, setAdmin] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const existingUser = localStorage.getItem("user");
     if (existingUser) {
-      const user = JSON.parse(existingUser)
-      setAdmin(user.role === "admin" )
+      const user = JSON.parse(existingUser);
+      setAdmin(user.role === "admin");
     }
-  }, [])
+  }, []);
+
+  const navigation = (page: string, path: string) => {
+    setActivePage(page);
+    navigate(path);
+  };
 
   return (
     <nav className="fixed bottom-0 sm:top-0 sm:h-20 flex items-center justify-between w-full sm:px-9 md:py-6 backdrop-blur-md bg-neutral-50/1">
-      <a href="/" className="hidden sm:block">
+      <a href="/home" className="hidden sm:block">
         <Logo />
       </a>
       <div className="flex flex-row justify-between gap-4 w-full sm:w-auto">
@@ -25,27 +32,29 @@ const Navbar: React.FC = () => {
           title={"Add subscription"}
           className="text-primary"
           isActive={activePage === "add-subscription"}
-          onClick={() => setActivePage("add-subscription")}
+          onClick={() => navigation("add-subscription", "/subscriptions")}
         ></NavbarButton>
+
         <NavbarButton
           icon={"ic:baseline-home"}
           title={"Home"}
           isActive={activePage === "home"}
-          onClick={() => setActivePage("home")}
+          onClick={() => navigation("home", "home")}
         ></NavbarButton>
+
         <NavbarButton
           icon={"streamline:subscription-cashflow"}
           title={"My subscriptions"}
           iconSize={32}
           isActive={activePage === "my-subscriptions"}
-          onClick={() => setActivePage("my-subscriptions")}
+          onClick={() => navigation("my-subscriptions", "/my-subscriptions")}
           className="px-2.5"
         ></NavbarButton>
         <NavbarButton
           icon={"mdi:user"}
           title={"Profile"}
           isActive={activePage === "profile"}
-          onClick={() => setActivePage("profile")}
+          onClick={() => navigation("profile", "/profile")}
         ></NavbarButton>
 
         {admin && (
@@ -53,7 +62,7 @@ const Navbar: React.FC = () => {
             icon={"dashicons:shield"}
             title={"Admin dashboard"}
             isActive={activePage === "admin"}
-            onClick={() => setActivePage("admin")}
+            onClick={() => navigation("admin", "/admin")}
           ></NavbarButton>
         )}
       </div>
