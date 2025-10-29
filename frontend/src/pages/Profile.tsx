47b66508feaf53c6513896d6d3ba1d5b96acf9cc
Lara <lara.salman01@hotmail.com>
2025-10-29 14:40:18 +0100

    feat: add subscribe toggle button

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../api/axios";
+import Button from "../components/Button";
 
 interface UserProfile {
   name: string;
@@ -13,18 +14,31 @@ const Profile: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
+  const fetchProfile = async () => {
+    try {
+      const res = await api.get("/users/profile");
+      setUser(res.data.user);
+    } catch (error) {
+      console.error(error);
+      setError("Failed to fetch profile");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const toggleSubscriber = async () => {
+    if (!user) return;
+
+    try {
+      const res = await api.patch("/users/subscribe");
+      setUser(res.data.user);
+    } catch (error) {
+      console.error(error);
+      setError("Failed to update role");
+    }
+  };
+
   useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const res = await api.get("/users/profile");
-        setUser(res.data.user);
-      } catch (error) {
-        console.error(error);
-        setError("Failed to fetch profile");
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchProfile();
   }, []);
 
@@ -35,13 +49,21 @@ const Profile: React.FC = () => {
     <div className="flex flex-col items-center pt-8 max-w-6xl mx-auto">
       <div className="flex flex-col items-center gap-3 px-6 py-9 w-full border border-neutral-400 shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] rounded-xl">
         {user && (
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-semibold">My profile</h1>
-            <img src={user.image} alt={user.name} className="size-16" />
-            <h2>Name: {user.name}</h2>
-            <p>E-mail: {user.email}</p>
-            <p>Role: {user.role}</p>
-          </div>
+          <>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-semibold">My profile</h1>
+              <img src={user.image} alt={user.name} className="size-16" />
+              <h2>Name: {user.name}</h2>
+              <p>E-mail: {user.email}</p>
+              <p>Role: {user.role}</p>
+            </div>
+
+            <Button onClick={toggleSubscriber}>
+              {user.role === "subscriber"
+                ? "Unsubscribe from e-mails"
+                : "Subscribe to e-mails"}
+            </Button>
+          </>
         )}
       </div>
     </div>
