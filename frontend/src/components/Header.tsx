2a6fd480c3df6f4be05b8c1c7eed17dfff55510f
Lara <lara.salman01@hotmail.com>
2025-10-29 16:20:30 +0100

    feat: add mobile only header in layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 flex items-center justify-center w-full py-2 backdrop-blur-md bg-neutral-50/1">
+    <header className="fixed top-0 flex items-center justify-center w-full py-2 backdrop-blur-md bg-neutral-50/1 sm:hidden">
       <Logo />
     </header>
   );
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Header from "./Header";
 
 const Layout = () => {
   return (
     <div className="flex flex-col items-center min-h-screen cursor-default">
       <Navbar />
+      <Header />
 
       <main className="w-full flex-1 p-20 sm:p-16 px-4">
         <Outlet />
