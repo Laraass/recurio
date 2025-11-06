a768d7d82933621aacfc345a822df460abeb203d
Lara <lara.salman01@hotmail.com>
2025-11-06 12:54:53 +0100

    style: apply w-full to containers

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -85,7 +85,7 @@ const Admin: React.FC = () => {
   return (
     <div className="flex flex-col items-center pt-8 max-w-6xl mx-auto">
       <div className="flex flex-col items-center sm:gap-3 sm:px-6 sm:py-9 w-full sm:border sm:border-neutral-400 sm:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] sm:rounded-xl">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 w-full">
           <h1 className="text-2xl font-bold">Admin dashboard</h1>
 
           <Searchbar
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center pt-8 max-w-6xl mx-auto">
-      <div className="flex flex-col items-center sm:gap-3 sm:px-6 sm:py-9 sm:w-full sm:border sm:border-neutral-400 sm:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] sm:rounded-xl">
+      <div className="flex flex-col items-center sm:gap-3 sm:px-6 sm:py-9 w-full sm:border sm:border-neutral-400 sm:shadow-[0_2px_4px_0_rgba(0,0,0,0.25)] sm:rounded-xl">
         <div className="flex flex-col gap-3 w-full max-w-110">
           <div className="flex gap-2">
             <h1 className="text-2xl font-semibold">Welcome back,</h1>
